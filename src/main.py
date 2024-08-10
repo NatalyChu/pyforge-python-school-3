1cf9f3c25d176036ed4c6bf3529562860f72cad5
qua_nyakhina <qua_nyakhina@exactsciences.com>
2024-08-11 01:02:01 +0200

    updated files according to linter

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, status
 from fastapi.exceptions import HTTPException
 from rdkit import Chem
+
 # from models import User
 
 
@@ -9,40 +10,54 @@ from rdkit import Chem
 app = FastAPI()
 
 molecules = [
-    {"mol_id": 1, "name": "CCO"}, 
-    {"mol_id": 2, "name": "c1ccccc1"}, 
-    {"mol_id": 3, "name": "CC(=O)O"}, 
-    {"mol_id": 4, "name": "CC(=O)Oc1ccccc1C(=O)O"}
-    ]
+    {"mol_id": 1, "name": "CCO"},
+    {"mol_id": 2, "name": "c1ccccc1"},
+    {"mol_id": 3, "name": "CC(=O)O"},
+    {"mol_id": 4, "name": "CC(=O)Oc1ccccc1C(=O)O"},
+]
+
 
 # List all molecules
 @app.get("/molecules", tags=["molecules"], summary="List all molecules")
 def list_all_molecules(skip: int = 0, limit: int = 10):
-    '''
+    """
     List all molecules
-    '''
+    """
     return molecules[skip : skip + limit]
 
+
 # Get molecule by identifier
-@app.get("/molecules/{mol_id}", tags=["molecules"], summary="Get molecule by identifier")
+@app.get(
+    "/molecules/{mol_id}", tags=["molecules"], summary="Get molecule by identifier"
+)
 def get_molecule_by_id(mol_id: int):
-    '''
+    """
     Get molecule by identifier
-    '''
+    """
     for mol in molecules:
         if mol["mol_id"] == mol_id:
             return mol
     raise HTTPException(status_code=404, detail="Molecule is not found")
 
+
 # Add molecule (smiles) and its identifier
-@app.post("/add_molecule", status_code=status.HTTP_201_CREATED, tags=["molecules"], response_description='Molecule is added')
+@app.post(
+    "/add_molecule",
+    status_code=status.HTTP_201_CREATED,
+    tags=["molecules"],
+    response_description="Molecule is added",
+)
 def add_molecule(molecule: dict):
     for mol in molecules:
         if mol["mol_id"] == molecule["mol_id"]:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Molecule with this ID already exists")
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Molecule with this ID already exists",
+            )
         else:
             molecules.append(molecule)
             return molecule
+
 
 # Updating a molecule by identifier
 @app.put("/molecules/{mol_id}", tags=["molecules"])
@@ -55,7 +70,11 @@ def update_molecule(mol_id: int, updated_mol: dict):
 
 
 # Delete a molecule by identifier
-@app.delete("/molecules/{mol_id}", tags=["molecules"], response_description='Molecule is deleted')
+@app.delete(
+    "/molecules/{mol_id}",
+    tags=["molecules"],
+    response_description="Molecule is deleted",
+)
 def delete_molecule(mol_id: int):
     for ind, mol in enumerate(molecules):
         if mol["mol_id"] == mol_id:
@@ -63,13 +82,18 @@ def delete_molecule(mol_id: int):
             return deleted_mol
     raise HTTPException(status_code=404, detail="Molecule is not found")
 
+
 # Substructure search for all added molecules
-@app.post("/molecules/sub_search", tags=["molecules"], response_description='List of all matching molecules')
+@app.post(
+    "/molecules/sub_search",
+    tags=["molecules"],
+    response_description="List of all matching molecules",
+)
 def find_substructure(sub_str: str):
-    '''
+    """
     Substructure search for all added molecules
     - **sub_str** - a SMILE string to search within all molecules
-    '''
+    """
     match_list = []
     try:
         sub_mol = Chem.MolFromSmiles(sub_str)
@@ -78,7 +102,7 @@ def find_substructure(sub_str: str):
             if smile_mol.HasSubstructMatch(sub_mol):
                 match_list.append(mol["name"])
         return match_list
-    except: 
-        raise HTTPException(status_code=422, detail="Provided string cannot be converted into molecule")
-    
-
+    except:
+        raise HTTPException(
+            status_code=422, detail="Provided string cannot be converted into molecule"
+        )
